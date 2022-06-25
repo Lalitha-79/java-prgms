1869aca6907a743c9d0db21837a0a59f34b418a7
acer <acer@192.168.0.104>
2022-06-25 12:48:38 +0530

    arthametic

---
+++ demo/src/demo/arithmatic.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class arithmatic {
+
+	public static void main(String[] args) {
+		int x = 30, y = 40;
+		System.out.println("arithmatic operator");
+		System.out.println(x+y);
+    	System.out.println(x-y);
+    	System.out.println("  ");
+    	System.out.println("relational operator");
+    	System.out.println(x<=y);
+    	System.out.println(x>=y);
+	}
+}
+    	
+    	
